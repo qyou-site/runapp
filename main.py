6aa148d083d16d6ccffad373f2fbf0f8b7c589a8
tenghoo <tenghoo@wiseai.tech>
2022-06-09 19:10:40 +0800

    fix db in main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from app import create_app, create_database
+from app import create_app, create_database,db
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
 DB_NAME = "database.db"
 app = create_app(DB_NAME,db)
 create_database(app,db)
