2a2646978010e10639f644da5c2115c941cefe42
tenghoo <tenghoo@wiseai.tech>
2022-06-10 17:59:30 +0800

    debug true

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = create_app(DB_NAME,db)
 create_database(app,db)
 
 if __name__ == '__main__':
-    app.run(debug=False,threaded=True,host="0.0.0.0", port=8080)
+    app.run(debug=True,threaded=True,host="0.0.0.0", port=8080)
