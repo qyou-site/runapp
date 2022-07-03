c05d7ad94d4c0d16964798c68d6093ac308a17d0
tenghoo <tenghoo@wiseai.tech>
2022-07-03 16:12:02 +0800

    add draw line function, link metadata with gsheets

--- utils/global_pose.py
+++ utils/global_pose.py
@@ -4,19 +4,16 @@ import pandas as pd
 import mediapipe as mp
 import numpy as np
 import cv2
+from datetime import datetime
+import gspread
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-def a(b):
-    return b
-
-c = a('test')
-
-
 class UtilsGen():
-    def __init__(self, landmarks):
+    def __init__(self, landmarks, height):
         self.landmarks = landmarks
+        self.height = height
     
     def calculate_angle(self, a, b, c):  # global utils
         a = np.array(a)  # First
@@ -41,10 +38,28 @@ class UtilsGen():
         y2 = b[0] * height
 
         dist = ((x1-x2)**2+(y1-y2)**2)**0.5
+
+        
         # if debug:
         #     cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)),
-        #              (255, 255, 0), thickness=5)
+        #              (255, 0, 0), thickness=1)
         return dist
+
+    def draw_line(self, point1, point2, img): 
+        width = img.shape[1]
+        height = img.shape[0]
+        y1 = point1[1] * height
+        x1 = point1[0] * width
+        y2 = point2[1] * height
+        x2 = point2[0] * width
+        # x1 = point1[1] * width
+        # y1 = point1[0] * height
+        # x2 = point2[1] * width
+        # y2 = point2[0] * height
+
+        cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)),
+                    (0, 255, 0), thickness=1)
+        return img
 
     def showText(self, metric, point, image, text, debug):  # global utils
         if debug:
@@ -53,9 +68,10 @@ class UtilsGen():
             cv2.putText(image, f'{text}: {metric}',
                         tuple(np.multiply(
                             point, [image.shape[1], image.shape[0]]).astype(int)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0,
-                                                      255, 0), 5, cv2.LINE_AA
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,
+                                                      255, 0), 1, cv2.LINE_AA
                         )
+        return image
 
     def greater_less_frame(self, landmark_coor):
         if landmark_coor[0] < 0 or landmark_coor[0] > 1:
@@ -74,7 +90,6 @@ class UtilsGen():
     def return_results(self, metrics, dict_to_save, frame):
         for key, value in dict_to_save.items():
             cv2.imwrite(os.path.join('app','static',frame,str(key)+'.png'), value)
-            print(os.listdir())
 # 'static'+frame+'/'+str(key)+'
         for key, value in metrics.items():
             print(f'{key} : {value}')
@@ -159,19 +174,76 @@ class UtilsGen():
 
         return left_ankle, left_heel, left_foot_index
 
-    def capture_metadata(self, video_type, filename, metrics, frame_angle):
-        if "metadata.csv" in os.listdir():
-            df = pd.read_csv('metadata.csv')
-        else:
-            df = pd.DataFrame()
+    def capture_metadata(self, video_type, filename, metrics, frame_angle, side_direction):
+        date = datetime.now().strftime("%Y/%m/%d-%H:%M:%S")
+      
+        sheet_id = '1YhmeuynXcs9Bo-Sl26HxsIkkrAbHdPMClxFh_nYVPvk'
+        sa = gspread.service_account(filename="newagent-pss9-9047ed9cb1b8.json")
+        sh = sa.open_by_url('https://docs.google.com/spreadsheets/d/1YhmeuynXcs9Bo-Sl26HxsIkkrAbHdPMClxFh_nYVPvk')
+       
+        main_sheet = 'metadata'
+        url_main = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={main_sheet}'
         
+        running_sheet = 'running'
+        url_run = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={running_sheet}'
+
+        squat_sheet = 'squat'
+        url_sqt = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={squat_sheet}'
+
+        try:
+            df = pd.read_csv(url_main)
+        except pd.errors.EmptyDataError as e:
+            df = pd.DataFrame()
         index = len(df.index)
+        df = df.dropna(axis=1,how='all')
+        
+        df.at[index,'date'] = date
         df.at[index,'filename'] = str(filename)+".png"
         df.at[index,'video_type'] = video_type
+        df.at[index,'height'] = self.height
         df.at[index,'frame'] = frame_angle
+        df.at[index,'side_direction'] = side_direction
         for metric in metrics:
             df.at[index, metric] = round(metrics[metric],2)
-        
-        df.to_csv('metadata.csv',index=False)
-        
+        df = df.fillna('')
+        main_wks = sh.worksheet('metadata')
+        main_wks.update([df.columns.values.tolist()] + df.values.tolist())
 
+        if video_type == 'running':
+            try:
+                df = pd.read_csv(url_run)
+            except pd.errors.EmptyDataError as e:
+                df = pd.DataFrame()
+            index = len(df.index)
+            df = df.dropna(axis=1,how='all')
+            
+            df.at[index,'date'] = date
+            df.at[index,'filename'] = str(filename)+".png"
+            df.at[index,'video_type'] = video_type
+            df.at[index,'height'] = self.height
+            df.at[index,'frame'] = frame_angle
+            df.at[index,'side_direction'] = side_direction
+            for metric in metrics:
+                df.at[index, metric] = round(metrics[metric],2)
+            df = df.fillna('')
+            run_wks = sh.worksheet('running')
+            run_wks.update([df.columns.values.tolist()] + df.values.tolist())
+        elif video_type == 'squat':
+            try:
+                df = pd.read_csv(url_sqt)
+            except pd.errors.EmptyDataError as e:
+                df = pd.DataFrame()
+            index = len(df.index)
+            df = df.dropna(axis=1,how='all')
+            
+            df.at[index,'date'] = date
+            df.at[index,'filename'] = str(filename)+".png"
+            df.at[index,'video_type'] = video_type
+            df.at[index,'height'] = self.height
+            df.at[index,'frame'] = frame_angle
+            df.at[index,'side_direction'] = side_direction
+            for metric in metrics:
+                df.at[index, metric] = round(metrics[metric],2)
+            df = df.fillna('')
+            squat_wks = sh.worksheet('squat')
+            squat_wks.update([df.columns.values.tolist()] + df.values.tolist())
