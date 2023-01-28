877ef781fd1be1aa7142d14d761a9737d1b5da5c
Vivek Dogra <vivekdogra94@gmail.com>
2023-01-28 15:09:19 +0530

    login signup api

--- app/__init__.py
+++ app/__init__.py
@@ -26,9 +26,10 @@ def create_app(DB_NAME,db):
 
     from .views import views
     from .auth import auth
-
+    from .api import api
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(api, url_prefix="/api")
 
     from .models import User
 
